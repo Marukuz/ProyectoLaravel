73b46fb9bfcc5b4b62ddb16b295c9ed51ebd2f93
Marc Garrido <marccocmc@gmail.com>
2023-01-20 09:00:19 +0100

    Base de datos

---
+++ install/empresa.sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-01-2023 a las 10:47:40
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `empresa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `cliente_id` int(11) NOT NULL,
+  `cif` varchar(45) DEFAULT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `telefono` int(11) DEFAULT NULL,
+  `correo` varchar(45) DEFAULT NULL,
+  `cuenta_corriente` varchar(45) DEFAULT NULL,
+  `pais` varchar(45) DEFAULT NULL,
+  `moneda` varchar(45) DEFAULT NULL,
+  `importe_mensual` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuotas`
+--
+
+CREATE TABLE `cuotas` (
+  `cuota_id` int(11) NOT NULL,
+  `concepto` varchar(45) DEFAULT NULL,
+  `fecha_emision` date DEFAULT NULL,
+  `importe` int(11) DEFAULT NULL,
+  `pagada` varchar(45) DEFAULT NULL,
+  `fecha_pago` date DEFAULT NULL,
+  `notas` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+CREATE TABLE `empleados` (
+  `empleado_id` int(11) NOT NULL,
+  `dni` varchar(45) DEFAULT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `correo` varchar(45) DEFAULT NULL,
+  `telefono` int(11) DEFAULT NULL,
+  `direccion` varchar(45) DEFAULT NULL,
+  `fecha_alta` date DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tareas`
+--
+
+CREATE TABLE `tareas` (
+  `tarea_id` int(11) NOT NULL,
+  `cliente_id` int(11) DEFAULT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `telefono` int(11) DEFAULT NULL,
+  `correo` varchar(45) DEFAULT NULL,
+  `direccion` varchar(45) DEFAULT NULL,
+  `poblacion` varchar(45) DEFAULT NULL,
+  `codigo_postal` int(11) DEFAULT NULL,
+  `provincia` varchar(45) DEFAULT NULL,
+  `estado_tarea` varchar(45) DEFAULT NULL,
+  `fecha_creacion` varchar(45) DEFAULT NULL,
+  `operario_encargado` varchar(45) DEFAULT NULL,
+  `fecha_realizacion` varchar(45) DEFAULT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  `anotacion_inicio` varchar(45) DEFAULT NULL,
+  `anotacion_final` varchar(45) DEFAULT NULL,
+  `Tareascol` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`cliente_id`);
+
+--
+-- Indices de la tabla `cuotas`
+--
+ALTER TABLE `cuotas`
+  ADD PRIMARY KEY (`cuota_id`);
+
+--
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`empleado_id`);
+
+--
+-- Indices de la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  ADD PRIMARY KEY (`tarea_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
