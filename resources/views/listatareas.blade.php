648c240ee6a26d7a63de0fe097b0fd564da32e66
Marc Garrido <marccocmc@gmail.com>
2023-01-20 09:27:51 +0100

    ListaTareas no funcional, solo vista.

---
+++ resources/views/listatareas.blade.php
@@ -0,0 +1,28 @@
+@extends('plantillaTareas')
+@section('contenido')
+  <div> 
+    <table class="table">
+      <thead class="table-dark">
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Apellido</th>
+          <th scope="col">Telefono</th>
+          <th scope="col">Poblacion</th>
+          <th scope="col">Provincia</th>
+          <th scope="col">Estado</th>
+          <th scope="col">Operario</th>
+          <th scope="col">Descripcion</th>
+          <th scope="col">Anotacion Inicial</th>
+          <th scope="col">Fecha de realizacion</th>
+          <th scope="col"></th>
+
+        </tr>
+      </thead>
+      <tbody>
+          <!-- Generacion de la tabla -->
+      
+      </tbody>
+    </table>
+  </div>
+@endsection
